cf959633dea4581a8198639107cc2a9872da3d0f
Xav83 <x.jouvenot@gmail.com>
2023-05-30 23:15:34 +0200

    [AU hidhide:1.2.128.0]

--- automatic/hidhide/tools/chocolateyInstall.ps1
+++ automatic/hidhide/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = '/exenoui'
+  silentArgs     = '/S'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
