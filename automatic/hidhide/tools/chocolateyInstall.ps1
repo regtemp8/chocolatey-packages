3d6200ac311858c192750712c7dbfc5e58d3e6cb
Xavier Jouvenot <x.jouvenot@gmail.com>
2023-06-19 22:28:14 +0200

    [au hidhide]

--- automatic/hidhide/tools/chocolateyInstall.ps1
+++ automatic/hidhide/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = '/qn'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
