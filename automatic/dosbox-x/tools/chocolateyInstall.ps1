2e9e1ca4b26fbf86e17b85f95789001d420c3436
Xavier Jouvenot <x.jouvenot@gmail.com>
2023-09-24 03:38:49 +0200

    [AU fanficfare dosbox-x]

--- automatic/dosbox-x/tools/chocolateyInstall.ps1
+++ automatic/dosbox-x/tools/chocolateyInstall.ps1
@@ -1,18 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32       = ''
 $url64       = ''
-$checksum32  = ''
 $checksum64  = ''
 
 $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
   fileType       = 'EXE'
-  url            = $url32
   url64Bit       = $url64
-  checksum       = $checksum32
   checksum64     = $checksum64
-  checksumType   = 'sha256'
   checksumType64 = 'sha256'
   silentArgs     = '/VERYSILENT /SP- /SUPPRESSMSGBOXES'
   validExitCodes = @(0)
