d847cbdd40418ec78b9aec817c66e9a463f89243
Xav83 <x.jouvenot@gmail.com>
2023-12-28 18:32:11 +0100

    Corrects the installer selected for super-productivity. Uses the x64 instead of the arm one.

--- automatic/super-productivity/update.ps1
+++ automatic/super-productivity/update.ps1
@@ -18,7 +18,7 @@ function global:au_GetLatest {
     $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
     $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
 
-    $re  = "superProductivity-Setup-.+.exe"
+    $re  = "superProductivity-Setup-.+x64.exe"
 
     $url = $assets_page.Links.Href -match $re | Select-Object -First 1
 
