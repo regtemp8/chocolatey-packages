f91b094d66942141c49e3d40ebe006d35f39e125
Xav83 <x.jouvenot@gmail.com>
2024-01-02 10:55:56 +0100

    [AU arrowdl]

---
+++ automatic/arrowdl/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
