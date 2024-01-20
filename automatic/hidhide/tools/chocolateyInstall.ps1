7df4c313612b485a2cbf0f7d9b9ee06241abb237
Xav83 <x.jouvenot@gmail.com>
2024-01-20 23:56:01 +0100

    Do not restart right after installing hidhide package.

--- automatic/hidhide/tools/chocolateyInstall.ps1
+++ automatic/hidhide/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = '/qn'
+  silentArgs     = '/qn /norestart'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
