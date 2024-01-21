4d9de8b51df6ebf3f77891aa68c5e9993d9a956f
Xav83 <x.jouvenot@gmail.com>
2024-01-21 16:19:25 +0100

    Fix the silent install option

--- automatic/antidupl/tools/chocolateyInstall.ps1
+++ automatic/antidupl/tools/chocolateyInstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = '-y'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
