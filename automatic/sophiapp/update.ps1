d8663b8f17a29f46523ccb0bf3e1e335668c1242
Xav83 <x.jouvenot@gmail.com>
2024-01-22 09:07:53 +0100

    Reverts the update script of sophiapp package in order to go back to deducing the version from the url.

--- automatic/sophiapp/update.ps1
+++ automatic/sophiapp/update.ps1
@@ -22,7 +22,7 @@ function global:au_GetLatest {
 
     $url = $assets_page.Links.Href -match $re | Select-Object -First 1
 
-    $version = '1.0.97.20240122'
+    $version = $url -split '/' | Select-Object -Last 1 -Skip 1
     $url64 = 'https://github.com' + $url
 
     $Latest = @{ URL64 = $url64; Version = $version }
