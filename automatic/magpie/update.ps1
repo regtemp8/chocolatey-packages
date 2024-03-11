15fb3daf22c7e3894fa3036d720e7720da09b675
Xav83 <x.jouvenot@gmail.com>
2024-03-11 18:50:52 +0100

    Adds support for magpie preview versions

--- automatic/magpie/update.ps1
+++ automatic/magpie/update.ps1
@@ -18,7 +18,7 @@ function global:au_GetLatest {
     $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
     $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
 
-    $re  = "Magpie-v.+-x64.zip" 
+    $re  = "Magpie-.+-x64.zip"
 
     $url = $assets_page.Links.Href -match $re | Select-Object -First 1
 
