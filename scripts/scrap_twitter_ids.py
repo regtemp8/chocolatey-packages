babe54748a4487f5ee53109953ef5d67f70f754b
Xav83 <x.jouvenot@gmail.com>
2024-07-15 00:40:25 +0200

    Adds a python script in order to generate a file to be use later with twitter-api-client library.

---
+++ scripts/scrap_twitter_ids.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentParser
+from twitter.scraper import Scraper
+
+parser = ArgumentParser(
+    prog="Twitter ids Scrapper",
+    description="Scraps the identifiant of Twitter to use with twitter-api-client -- https://github.com/trevorhobenshield/twitter-api-client",
+    epilog="",
+)
+
+parser.add_argument(
+    "email", metavar="package", type=str, nargs=1, help="email of the Twitter account"
+)
+
+parser.add_argument(
+    "username",
+    metavar="package",
+    type=str,
+    nargs=1,
+    help="username of the Twitter account",
+)
+
+parser.add_argument(
+    "password",
+    metavar="package",
+    type=str,
+    nargs=1,
+    help="password of the Twitter account",
+)
+
+args = parser.parse_args()
+
+scraper = Scraper(args.email[0], args.username[0], args.password[0])
+scraper.save_cookies("twitter_ids")
